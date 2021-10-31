7e1acdcd9e896daac0ef953811512a1cea2d4ed8
cmorgan333 <78804785+cmorgan333@users.noreply.github.com>
2021-10-31 02:20:29 -0400

    forms
    forms

--- app/app.js
+++ app/app.js
@@ -1,25 +1,39 @@
-function changeRoute() {
-    let hashTag = window.location.hash;
-    let pageID = hashTag.replace("#", "");
+// function changeRoute() {
+//     let hashTag = window.location.hash;
+//     let pageID = hashTag.replace("#", "");
 
-MODEL.updateView(pageID)
-}
+// MODEL.updateView(pageID)
+// }
 
-function initURLListener() {
-    $(window).on("hashchange", changeRoute);
-    changeRoute();
-}
+// function initURLListener() {
+//     $(window).on("hashchange", changeRoute);
+//     changeRoute();
+// }
 
 function initListeners() {
     $("#create").click(function (e) {
         e.preventDefault();
 
         let firstName = $("#fName").val();
-        console.log("firstName " + firstName);
+        let email = $("#email").val();
+        let hobby1 = $("#hobby1").val();
+        let hobby2 = $("#hobby2").val();
+        let hobby3 = $("#hobby3").val();
+        let game1 = $("#game1").val();
+        let game2 = $("#game2").val();
+        let game3 = $("#game3").val();
+        let book1 = $("#book1").val();
+        let book2 = $("#book2").val();
+
+   console.log("inputs " + firstName + " " + email + " " + hobby1 + " " + hobby2 + " " + hobby3 + 
+   " " + " " + game1 + " " + game2 + " " + game3 + " " + book1 + " " + book2);
+
     });
 }
 
+
+
+
 $(document).ready(function() {
-    initURLListener();
     initListeners(); 
 });
